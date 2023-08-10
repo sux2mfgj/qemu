8e62022bdc5c22aae7decbb98fea77fcfc433e57
Shunsuke Mie <mie@igel.co.jp>
2023-08-10 09:52:01 +0900

    add header

---
+++ hw/misc/qemu-epc.h
@@ -0,0 +1,34 @@
+#ifndef QEMU_EPC_H
+#define QEMU_EPC_H
+
+#include <stdint.h>
+
+#define QEMU_EPC_PROTOCOL_VERSION 0xdeadbeef
+#define QEMU_EPC_SOCK_PATH "/tmp/qemu-epc.sock"
+
+enum {
+    QEMU_EPC_MSG_TYPE_VER,
+    QEMU_EPC_MSG_TYPE_HDR,
+    QEMU_EPC_MSG_TYPE_BAR,
+};
+
+struct qemu_epc_req_hdr {
+    uint32_t type;
+};
+
+struct qemu_epc_req_pci_config {
+    uint32_t offset;
+    uint32_t size;
+} __attribute__((packed));
+
+#define QEMU_EPC_REQ_BAR_MASK 1
+#define QEMU_EPC_REQ_BAR_BAR 2
+struct qemu_epc_resp_bar {
+    uint64_t size;
+};
+
+struct qemu_epc_req_bar {
+    uint8_t type;
+};
+
+#endif /* QEMU_EPC_H */
